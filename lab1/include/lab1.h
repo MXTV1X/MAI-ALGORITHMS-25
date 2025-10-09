474c0420dfa5f1bb46b13442f979cbe38563c46a
Matvey Labutin <1matvey1579@gmail.com>
2025-10-09 18:20:01 +0000

    correct laba 1

---
+++ lab1/include/lab1.h
@@ -0,0 +1,27 @@
+#ifndef _LAB1
+#define _LAB1
+
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef enum {
+    INCORRECT_INPUT,
+    OVERFLOW,
+    NEGATIVE_NUMBER,
+    INCORRECT_FLAG
+} BadType;
+
+void Multiple(int x);
+void Divisibility(int x);
+void ConvertToHex(int x);
+uint64_t power(int x, int n);
+void Degree(int x);
+uint64_t Sum(int x);
+uint64_t Factorial(int x);
+
+#endif
